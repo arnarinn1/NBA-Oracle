216199bca3bdb9de79bd9742cb32c9c8e7a4db49
arnarinn1 <arnarh91@gmail.com>
2020-12-09 20:49:02 +0000

    Improve unit test setup & coverage for playbyplay

---
+++ src/NbaOracle.Tests/Unit/Fixtures/DocumentFixture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Dom;
+
+namespace NbaOracle.Tests.Unit.Fixtures
+{
+    public class DocumentFixture : IDisposable
+    {
+        public async Task<IDocument> CreateDocument(string resource)
+        {
+            var context = BrowsingContext.New(Configuration.Default.WithDefaultLoader());
+            var content = await ReadEmbeddedResource(resource);
+            return await context.OpenAsync(request => request.Content(content));
+        }
+
+        private async Task<string> ReadEmbeddedResource(string name)
+        {
+            var resourceStream = GetType().GetTypeInfo().Assembly.GetManifestResourceStream(name);
+
+            if (resourceStream == null)
+                throw new FileNotFoundException($"Embedded resource not found for name = {name}");
+
+            using var reader = new StreamReader(resourceStream);
+            return await reader.ReadToEndAsync();
+        }
+
+        public void Dispose() { }
+    }
+}
--- src/NbaOracle.Tests/Unit/Providers/Teams/Parsers/PlayByPlay/PlayByPlayParserTests.cs
+++ src/NbaOracle.Tests/Unit/Providers/Teams/Parsers/PlayByPlay/PlayByPlayParserTests.cs
@@ -1,28 +1,35 @@
 ﻿using System.Linq;
-using System.Threading.Tasks;
-using AngleSharp;
+using AngleSharp.Dom;
 using FluentAssertions;
 using NbaOracle.Providers.BasketballReference.Teams.Parsers.PlayByPlay;
+using NbaOracle.Tests.Unit.Fixtures;
 using Xunit;
 
 namespace NbaOracle.Tests.Unit.Providers.Teams.Parsers.PlayByPlay
 {
-    public class PlayByPlayParserTests : UnitBase
+    public class PlayByPlayParserTests : UnitBase, IClassFixture<DocumentFixture>
     {
-        private const string EmbeddedResourceLocation = "NbaOracle.Tests.Unit.Providers.Teams.Parsers.PlayByPlay.play_by_play_example_html_data.txt"; //fix this 
+        private readonly IDocument _document;
+        private readonly PlayByPlayParser _parser;
+
+        public PlayByPlayParserTests(DocumentFixture fixture)
+        {
+            _document = fixture.CreateDocument("NbaOracle.Tests.Unit.Providers.Teams.Parsers.PlayByPlay.play_by_play_example_html_data.txt").GetAwaiter().GetResult();
+            _parser = new PlayByPlayParser();
+        }
 
         [Fact]
-        public async Task Parse_ShouldParseHtml_WhenPlayByPlayIsPresent()
+        public void Parse_ShouldParse_WhenDocumentIsValid()
         {
-            var context = BrowsingContext.New(Configuration.Default.WithDefaultLoader());
-            var parser = new PlayByPlayParser();
+            var output = _parser.Parse(_document).ToArray();
 
-            var content = await ReadEmbeddedResource(EmbeddedResourceLocation);
-            var document = await context.OpenAsync(request => request.Content(content));
-
-            var output = parser.Parse(document);
+            var player = output.Single(x => x.PlayerName == "LeBron James");
 
             output.Count().Should().Be(20);
+            player.Games.Should().Be(67);
+            player.MinutesPlayed.Should().Be(2316);
+            player.PlusMinusOnCourt.Should().Be(9.0);
+            player.PlusMinusNetOnOffCourt.Should().Be(9.9);
         }
     }
 }
