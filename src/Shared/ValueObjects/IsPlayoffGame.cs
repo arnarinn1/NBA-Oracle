af1f586ae1660ebe5f9340bac39d6ae35e32ac59
arnarinn1 <arnarh91@gmail.com>
2020-12-11 20:53:53 +0000

    Fix years in IsPlayoffGame

--- src/Shared/ValueObjects/IsPlayoffGame.cs
+++ src/Shared/ValueObjects/IsPlayoffGame.cs
@@ -21,29 +21,29 @@ namespace ValueObjects
             switch (gameDate.Year)
             {
                 case 2020:
-                    return gameDate >= new DateTime(2020,08, 17) && gameDate <= new DateTime(2020, 10, 11);
+                    return gameDate >= new DateTime(2020, 8, 17) && gameDate <= new DateTime(2020, 10, 11);
                 case 2019:
                     return gameDate >= new DateTime(2019, 4, 13) && gameDate <= new DateTime(2019, 6, 13);
                 case 2018:
-                    return gameDate >= new DateTime(2019, 4, 14) && gameDate <= new DateTime(2019, 6, 8);
+                    return gameDate >= new DateTime(2018, 4, 14) && gameDate <= new DateTime(2018, 6, 8);
                 case 2017:
-                    return gameDate >= new DateTime(2019, 4, 15) && gameDate <= new DateTime(2019, 6, 12);
+                    return gameDate >= new DateTime(2017, 4, 15) && gameDate <= new DateTime(2017, 6, 12);
                 case 2016:
-                    return gameDate >= new DateTime(2019, 4, 16) && gameDate <= new DateTime(2019, 6, 19);
+                    return gameDate >= new DateTime(2016, 4, 16) && gameDate <= new DateTime(2016, 6, 19);
                 case 2015:
-                    return gameDate >= new DateTime(2019, 4, 18) && gameDate <= new DateTime(2019, 6, 16);
+                    return gameDate >= new DateTime(2015, 4, 18) && gameDate <= new DateTime(2015, 6, 16);
                 case 2014:
-                    return gameDate >= new DateTime(2019, 4, 19) && gameDate <= new DateTime(2019, 6, 15);
+                    return gameDate >= new DateTime(2014, 4, 19) && gameDate <= new DateTime(2014, 6, 15);
                 case 2013:
-                    return gameDate >= new DateTime(2019, 4, 20) && gameDate <= new DateTime(2019, 6, 20);
+                    return gameDate >= new DateTime(2013, 4, 20) && gameDate <= new DateTime(2013, 6, 20);
                 case 2012:
-                    return gameDate >= new DateTime(2019, 4, 28) && gameDate <= new DateTime(2019, 6, 21);
+                    return gameDate >= new DateTime(2012, 4, 28) && gameDate <= new DateTime(2012, 6, 21);
                 case 2011:
-                    return gameDate >= new DateTime(2019, 4, 16) && gameDate <= new DateTime(2019, 6, 12);
+                    return gameDate >= new DateTime(2011, 4, 16) && gameDate <= new DateTime(2011, 6, 12);
                 case 2010:
-                    return gameDate >= new DateTime(2019, 4, 17) && gameDate <= new DateTime(2019, 6, 17);
+                    return gameDate >= new DateTime(2010, 4, 17) && gameDate <= new DateTime(2010, 6, 17);
                 case 2009:
-                    return gameDate >= new DateTime(2019, 4, 18) && gameDate <= new DateTime(2019, 6, 14);
+                    return gameDate >= new DateTime(2009, 4, 18) && gameDate <= new DateTime(2009, 6, 14);
                 default:
                     throw new ArgumentException($"Playoff starting date is not known for year = {gameDate.Year}");
             }
