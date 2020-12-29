deef8b27334e46e045a481dbdf4030b1cada7018
arnarinn1 <arnarh91@gmail.com>
2020-12-29 17:24:04 +0000

    Change MergePlayer procedure to return player id

--- sql/StoredProcedures/sp_MergePlayers.sql
+++ sql/StoredProcedures/sp_MergePlayers.sql
@@ -4,37 +4,43 @@ CREATE OR ALTER Procedure nba.sp_MergePlayers
 AS 
 BEGIN
   SET NOCOUNT ON;
-   
-   MERGE nba.Player AS target
-   USING 
-   ( SELECT Identifier
-          , Name
-          , BirthDate
-          , BirthCountry
-          , College
-       FROM @Players
-   ) AS source 
-   ( Identifier
-   , Name
-   , BirthDate
-   , BirthCountry
-   , College
-   )
-   ON (target.Identifier = source.Identifier)
-   WHEN NOT MATCHED THEN 
-   INSERT
-   ( Identifier
-   , Name
-   , BirthDate
-   , BirthCountry
-   , College
-   )
-   VALUES 
-   ( source.Identifier
-   , source.Name
-   , source.BirthDate
-   , source.BirthCountry
-   , source.College
-   );
+ 
+  MERGE nba.Player AS target
+  USING 
+  ( SELECT Identifier
+         , Name
+         , BirthDate
+         , BirthCountry
+         , College
+      FROM @Players
+  ) AS source 
+  ( Identifier
+  , Name
+  , BirthDate
+  , BirthCountry
+  , College
+  )
+  ON (target.Identifier = source.Identifier)
+  WHEN NOT MATCHED THEN 
+  INSERT
+  ( Identifier
+  , Name
+  , BirthDate
+  , BirthCountry
+  , College
+  )
+  VALUES 
+  ( source.Identifier
+  , source.Name
+  , source.BirthDate
+  , source.BirthCountry
+  , source.College
+  );
+
+  SELECT p.Id              PlayerId 
+       , p.Identifier      PlayerIdentifier
+    FROM @Players pt
+    JOIN nba.Player p
+      ON p.identifier = pt.identifier
 END
 GO
